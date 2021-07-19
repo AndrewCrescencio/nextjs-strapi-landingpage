f6bb4fe0124e8fb2f85cbea8a06e6098dff305a9
Andrew Crescencio <andrew_workstation@hotmail.com>
2021-07-19 16:42:56 -0300

    all migrated to typescript

--- src/components/GridTwoColumn/mock.js
+++
@@ -1,5 +0,0 @@
-export default {
-  title: 'Grid Two Column',
-  text: `Lorem, ipsum dolor sit amet consectetur adipisicing elit. A, soluta illo eos quae veniam, nam ea necessitatibus pariatur quibusdam voluptatibus earum labore exercitationem. Aliquam asperiores repellendus neque beatae fuga quod`,
-  srcImg: 'assets/images/javascript.svg',
-};
